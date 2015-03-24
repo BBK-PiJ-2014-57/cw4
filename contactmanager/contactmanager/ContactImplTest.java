51c6727bdcc07b9d3897db494c8a380a076246df
Lewis Palmer <lewispalmer@Lewiss-MacBook-Pro.local>
2015-03-24 19:04:32 +0000

    Changes to JUnit Tests for Contact Interface

--- contactmanager/contactmanager/ContactImplTest.java
+++ contactmanager/contactmanager/ContactImplTest.java
@@ -1,5 +1,76 @@
 package contactmanager;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ContactImplTest {
 
+	private Contact testContact;
+	private Contact testContact2;
+	private Contact copytestContact;
+	private Contact emptyNoteContact;
+	private String testName = "Michael";
+	private String testNote = "Test";
+	private Contact noNoteCtorContact;
+	private Contact firstContact;
+	private Contact secondContact;
+	@Before
+	public void SetUpContact()
+	{
+		testContact = new ContactImpl(testName, testNote);
+		testContact2 = new ContactImpl("Bruce", testNote);
+		copytestContact = new ContactImpl("Harry", testNote);
+		emptyNoteContact = new ContactImpl(testName, "");
+		noNoteCtorContact = new ContactImpl("Joe");
+		
+	}
+	
+	@Test
+	public void getIdTest(){
+		int count = ContactImpl.totalContacts;
+		firstContact = new ContactImpl(testName, testNote);
+		assertEquals(firstContact.getId(), count);
+	}
+	
+	@Test
+	public void getIdMultipleContactsTest(){
+		int count = ContactImpl.totalContacts;
+		firstContact = new ContactImpl(testName, testNote);
+		secondContact = new ContactImpl(testName, testNote);
+		assertEquals(secondContact.getId(), count + 1);
+	}
+	
+	@Test
+	public void getIdDistinctIDTest(){
+		assertFalse(testContact.getId() == copytestContact.getId());
+	}
+	
+	@Test
+	public void getNameTest(){
+		assertEquals(testContact.getName(), testName);
+	}
+	
+	@Test
+	public void getNotesTest(){
+		assertEquals(testContact.getNotes(), testNote);
+	}
+	
+	@Test
+	public void getNotesNoNoteTest(){
+		assertEquals(noNoteCtorContact.getNotes(), "");
+	}
+	
+	@Test
+	public void addNotesTest(){
+		emptyNoteContact.addNotes(testNote);
+		assertEquals(testContact.getNotes(), testNote);
+	}
+	
+	@Test
+	public void addNotesnullNoteTest(){
+		noNoteCtorContact.addNotes(null);
+		assertEquals(noNoteCtorContact.getNotes(), "");
+	}
 }
